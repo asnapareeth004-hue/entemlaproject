c8558dc6c911a0fb9454f57cc24253c913a72f1a
asnapareeth004-hue <asnapareeth004@gmail.com>
2026-05-06 18:09:37 +0530

    status deleted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "../components/home/Navbar";
 import Hero from "../components/home/Hero";
-import Stats from "../components/home/Stats";
 import Trending from "../components/home/Trending";
 import { useNavigate } from "react-router-dom";
 
@@ -11,7 +10,7 @@ const Home = () => {
     <div>
       <Navbar />
       <Hero />
-      <Stats />
+      
       <Trending />
       <button onClick={() =>  navigate("/role")}>
         Login / Get Started
