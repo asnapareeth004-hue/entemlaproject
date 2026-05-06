472498871810b1b380160e81b5a20fd650c6922a
asnapareeth004-hue <asnapareeth004@gmail.com>
2026-05-06 18:24:08 +0530

    filecomp updted

--- src/components/home/Stats.jsx
+++ src/components/home/Stats.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import "./Stats.css";
+import { useNavigate } from "react-router-dom";
 
 const Stats = () => {
+  const navigate = useNavigate();
   return (
     <div className="stats-container">
 
       
       
 
-      <button className="complaint-btn">
+      <button className="complaint-btn" onClick={() => navigate("/login")}>
         + File Complaint
       </button>
 
